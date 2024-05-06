cef68543eaeda53f9e45c4b46449a234ab0012b4
Andrey Rusakov <psihatter@gmail.com>
2024-05-06 21:20:29 +0700

    Postgres implementation.

--- go.mod
+++ go.mod
@@ -1,16 +1,50 @@
 module github.com/psyhatter/tablegen
 
-go 1.20
+go 1.22
 
 require (
-	cloud.google.com/go/spanner v1.45.0
+	cloud.google.com/go/spanner v1.61.0
+	github.com/docker/docker v26.1.1+incompatible
+	github.com/docker/go-connections v0.5.0
+	github.com/google/uuid v1.6.0
+	github.com/jackc/pgx/v5 v5.5.5
+	github.com/pkg/errors v0.9.1
+	github.com/pressly/goose/v3 v3.20.0
 	github.com/rinchsan/gosimports v0.3.8
-	golang.org/x/sync v0.1.0
+	golang.org/x/sync v0.7.0
+	golang.org/x/text v0.14.0
 )
 
 require (
-	cloud.google.com/go v0.110.2 // indirect
+	cloud.google.com/go v0.112.2 // indirect
+	github.com/Microsoft/go-winio v0.4.14 // indirect
+	github.com/containerd/log v0.1.0 // indirect
+	github.com/distribution/reference v0.6.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/go-logr/logr v1.4.1 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	github.com/mfridman/interpolate v0.0.2 // indirect
+	github.com/moby/docker-image-spec v1.3.1 // indirect
+	github.com/moby/term v0.5.0 // indirect
+	github.com/morikuni/aec v1.0.0 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.0 // indirect
+	github.com/sethvargo/go-retry v0.2.4 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.49.0 // indirect
+	go.opentelemetry.io/otel v1.26.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.26.0 // indirect
+	go.opentelemetry.io/otel/metric v1.26.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.26.0 // indirect
+	go.opentelemetry.io/otel/trace v1.26.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/crypto v0.22.0 // indirect
 	golang.org/x/mod v0.9.0 // indirect
-	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
 	golang.org/x/tools v0.7.0 // indirect
+	gotest.tools/v3 v3.5.1 // indirect
 )
---
+++ pg/main.go
@@ -0,0 +1,607 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	_ "embed"
+	"flag"
+	"io"
+	"log/slog"
+	"net"
+	"net/url"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"text/template"
+	"time"
+
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/image"
+	"github.com/docker/docker/client"
+	"github.com/docker/go-connections/nat"
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/jackc/pgx/v5/stdlib"
+	"github.com/pkg/errors"
+	"github.com/pressly/goose/v3"
+	"github.com/rinchsan/gosimports"
+	"golang.org/x/sync/errgroup"
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
+)
+
+var (
+	dst = flag.String("dst", "internal/store/postgres/tables", "where to save models")
+
+	rawInitialisms = flag.String("initialisms", "", "comma separated initialisms")
+
+	migrationPath = flag.String(
+		"migrationPath",
+		".db/postgres/migrations",
+		"to specify migration files folder",
+	)
+
+	logger = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{AddSource: true}))
+)
+
+func main() {
+	flag.Parse()
+
+	for _, s := range strings.Split(*rawInitialisms, ",") {
+		initialisms[s] = struct{}{}
+	}
+
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts()
+	if err != nil {
+		panic(err)
+	}
+
+	creds := ("tablegen_" + uuid.NewString())[:20]
+
+	db, containerID, err := SetUpContainer(ctx, cli, creds)
+	if err != nil {
+		panic(err)
+	}
+
+	defer RemoveContainer(ctx, cli, containerID)
+
+	err = RunMigrations(ctx, db, *migrationPath)
+	if err != nil {
+		panic(err)
+	}
+
+	tables, err := GetTables(ctx, db)
+	if err != nil {
+		panic(err)
+	}
+
+	primaryKeys, err := GetPrimaryKeys(ctx, db)
+	if err != nil {
+		panic(err)
+	}
+
+	data, err := CreateFileData(tables, primaryKeys)
+	if err != nil {
+		panic(err)
+	}
+
+	err = WriteFiles(*dst, data)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func SetUpContainer(ctx context.Context, cli *client.Client, creds string) (db *pgxpool.Pool, containerID string, err error) {
+	defer func() {
+		if containerID != "" && err != nil {
+			RemoveContainer(ctx, cli, containerID)
+		}
+	}()
+
+	imageName := "postgres"
+
+	out, err := cli.ImagePull(ctx, imageName, image.PullOptions{})
+	if err != nil {
+		return nil, "", errors.WithStack(err)
+	}
+
+	defer func() {
+		if err := out.Close(); err != nil {
+			logger.Error("closing read closer", slog.Any("err", err))
+		}
+	}()
+
+	_, err = io.Copy(os.Stdout, out)
+	if err != nil {
+		logger.Error("copying reader", slog.Any("err", err))
+	}
+
+	resp, err := cli.ContainerCreate(
+		ctx,
+		&container.Config{
+			Image: imageName,
+			Cmd: []string{
+				"postgres",
+				"-c", "fsync=off",
+				"-c", "shared_buffers=2048MB",
+				"-c", "max_locks_per_transaction=1024",
+				"-c", "synchronous_commit=off",
+				"-c", "fsync=false",
+				"-c", "full_page_writes=false",
+				"-c", "log_statement=none",
+				"-c", "max_wal_size=5GB",
+				"-c", "maintenance_work_mem=2GB",
+				"-c", "wal_level=minimal",
+				"-c", "archive_mode=off",
+				"-c", "max_wal_senders=0",
+			},
+			Env: []string{
+				"POSTGRES_DB=" + creds,
+				"POSTGRES_USER=" + creds,
+				"POSTGRES_PASSWORD=" + creds,
+			},
+			ExposedPorts: nat.PortSet{
+				"5432/tcp": struct{}{},
+			},
+			// TODO: Figure out why it doesn't work.
+			// Healthcheck: &container.HealthConfig{
+			// 	Test:     []string{"/usr/bin/pg_isready", "-h", "localhost", "-p", "5432"},
+			// 	Interval: 100 * time.Millisecond,
+			// 	Timeout:  10 * time.Second,
+			// },
+		},
+		&container.HostConfig{
+			PortBindings: nat.PortMap{
+				"5432/tcp": []nat.PortBinding{
+					{
+						HostIP:   "localhost",
+						HostPort: "0",
+					},
+				},
+			},
+		},
+		nil,
+		nil,
+		creds,
+	)
+	if err != nil {
+		return nil, "", errors.WithStack(err)
+	}
+
+	err = cli.ContainerStart(ctx, resp.ID, container.StartOptions{})
+	if err != nil {
+		return nil, resp.ID, errors.WithStack(err)
+	}
+
+	logs, err := cli.ContainerLogs(
+		ctx,
+		resp.ID,
+		container.LogsOptions{ShowStdout: true, ShowStderr: true, Timestamps: true},
+	)
+	if err != nil {
+		logger.Error("getting logs", slog.Any("err", err))
+	}
+
+	defer func() {
+		if err := logs.Close(); err != nil {
+			logger.Error("closing read closer", slog.Any("err", err))
+		}
+	}()
+
+	_, err = io.Copy(os.Stdout, logs)
+	if err != nil {
+		logger.Error("copying reader", slog.Any("err", err))
+	}
+
+	info, err := cli.ContainerInspect(ctx, resp.ID)
+	if err != nil {
+		return nil, "", errors.WithStack(err)
+	}
+
+	port := info.NetworkSettings.Ports["5432/tcp"][0].HostPort
+
+	dsn := url.URL{
+		Scheme: "postgres",
+		User:   url.UserPassword(creds, creds),
+		Host:   net.JoinHostPort("localhost", port),
+		Path:   creds,
+	}
+
+	db, err = pgxpool.New(ctx, dsn.String())
+	if err != nil {
+		return nil, "", errors.WithStack(err)
+	}
+
+	start := time.Now()
+	ticker := time.NewTicker(100 * time.Millisecond)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		err = db.Ping(ctx)
+		if err == nil {
+			break
+		}
+
+		if time.Since(start) > 10*time.Second {
+			return nil, "", errors.Wrapf(err, "connecting to the db")
+		}
+	}
+
+	return db, resp.ID, nil
+}
+
+func RemoveContainer(ctx context.Context, cli *client.Client, containerID string) {
+	err := cli.ContainerStop(ctx, containerID, container.StopOptions{})
+	if err != nil {
+		logger.Error("stopping container", slog.Any("err", err))
+	}
+
+	err = cli.ContainerRemove(ctx, containerID, container.RemoveOptions{})
+	if err != nil {
+		logger.Error("removing container", slog.Any("err", err))
+	}
+}
+
+func RunMigrations(ctx context.Context, db *pgxpool.Pool, path string) error {
+	err := goose.RunContext(ctx, "up", stdlib.OpenDBFromPool(db), path)
+	return errors.WithStack(err)
+}
+
+func GetTables(ctx context.Context, db *pgxpool.Pool) (map[Table][]Column, error) {
+	q := `
+	SELECT
+	    table_schema,
+	    table_name,
+	    column_name,
+	    column_default,
+	    is_nullable::BOOL,
+	    udt_name,
+	    character_maximum_length
+	FROM information_schema.columns
+	WHERE table_schema NOT IN ('information_schema', 'pg_catalog')
+	ORDER BY table_name, LENGTH(column_name), column_name;`
+
+	rows, err := db.Query(ctx, q)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	defer rows.Close()
+
+	var t Table
+	var c Column
+	m := map[Table][]Column{}
+	for rows.Next() {
+		err = rows.Scan(&t.Schema, &t.Name, &c.Name, &c.Default, &c.Nullable, &c.Type, &c.CharLength)
+		if err != nil {
+			return nil, errors.WithStack(err)
+		}
+
+		m[t] = append(m[t], c)
+	}
+
+	return m, errors.WithStack(rows.Err())
+}
+
+func GetPrimaryKeys(ctx context.Context, db *pgxpool.Pool) (map[Table][]string, error) {
+	q := `
+	SELECT i.indrelid::REGCLASS, a.attname
+	FROM pg_index i
+	JOIN pg_attribute a ON a.attrelid = i.indrelid AND a.attnum = ANY (i.indkey)
+	WHERE i.indisprimary
+	ORDER BY i.indrelid, LENGTH(a.attname), a.attname;`
+
+	rows, err := db.Query(ctx, q)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	defer rows.Close()
+
+	var t Table
+	var c string
+	m := map[Table][]string{}
+	for rows.Next() {
+		err = rows.Scan(&t.Name, &c)
+		if err != nil {
+			return nil, errors.WithStack(err)
+		}
+
+		t.Schema = "public"
+		if strings.Contains(t.Name, ".") {
+			schemaAndName := strings.SplitN(t.Name, ".", 2)
+			t.Schema, t.Name = schemaAndName[0], schemaAndName[1]
+		}
+
+		m[t] = append(m[t], c)
+	}
+
+	return m, errors.WithStack(rows.Err())
+}
+
+//go:embed templates/main_file.go
+var mainFile []byte
+
+//go:embed templates/tables.gohtml
+var tablesTemplate string
+
+var templateFuncs = template.FuncMap{
+	"snakeToCamelCase": snakeToCamelCase,
+	"selectColumnType": SelectColumnType,
+	"insertColumnType": InsertColumnType,
+	"updateColumnType": UpdateColumnType,
+	"columnType":       ColumnType,
+}
+
+func CreateFileData(tables map[Table][]Column, primaryKeys map[Table][]string) (map[string][]byte, error) {
+	fileName := "tables.go"
+	b, err := gosimports.Process(fileName, mainFile, nil)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	files := make(map[string][]byte, len(tables)+1)
+	files[fileName] = b
+
+	templ, err := template.New("").Funcs(templateFuncs).Parse(tablesTemplate)
+	if err != nil {
+		return files, errors.WithStack(err)
+	}
+
+	var buf bytes.Buffer
+	for table, columns := range tables {
+		snakelike := strings.Replace(table.GetName(), ".", "_", 1)
+		fileName = snakelike + ".go"
+		if _, exist := files[fileName]; exist {
+			for i := 2; true; i++ {
+				fileName = snakelike + strconv.Itoa(i) + ".go"
+				if _, exist = files[fileName]; !exist {
+					break
+				}
+			}
+		}
+
+		buf.Reset()
+		err = table.Write(templ, &buf, primaryKeys[table], columns)
+		if err != nil {
+			return files, err
+		}
+
+		b = append([]byte{}, buf.Bytes()...)
+		b, err = gosimports.Process(fileName, b, nil)
+		if err != nil {
+			return files, errors.Wrapf(err, "formatting %q: %s", fileName, buf.Bytes())
+		}
+
+		files[fileName] = b
+	}
+
+	return files, nil
+}
+
+func WriteFiles(dst string, files map[string][]byte) error {
+	err := os.RemoveAll(dst)
+	if err != nil {
+		return errors.Wrapf(err, "removing %q", dst)
+	}
+
+	err = os.MkdirAll(dst, 0750)
+	if err != nil {
+		return errors.Wrapf(err, "creating %q", dst)
+	}
+
+	var g errgroup.Group
+	g.SetLimit(20)
+
+	for fileName, data := range files {
+		fileName, data := fileName, data
+		g.Go(func() error {
+			return writeFile(filepath.Join(dst, fileName), data)
+		})
+	}
+
+	return g.Wait()
+}
+
+func writeFile(fileName string, data []byte) error {
+	file, err := os.Create(fileName)
+	if err != nil {
+		return errors.Wrapf(err, "creating %q", fileName)
+	}
+
+	defer file.Close()
+
+	_, err = file.Write(data)
+	return errors.Wrapf(err, "writing to %q", fileName)
+}
+
+type Table struct{ Schema, Name string }
+
+func (t Table) GetName() string {
+	if t.Schema == "public" {
+		return t.Name
+	}
+	return t.Schema + "." + t.Name
+}
+
+func (t Table) Write(templ *template.Template, buf *bytes.Buffer, primaryKeys []string, cols []Column) error {
+	if len(cols) == 0 {
+		return nil
+	}
+
+	primary := make(map[string]struct{}, len(primaryKeys))
+	for _, key := range primaryKeys {
+		primary[key] = struct{}{}
+	}
+
+	for i := range cols {
+		_, cols[i].IsPrimaryKey = primary[cols[i].Name]
+	}
+
+	tableName := t.GetName()
+	camelCasedTableName := snakeToCamelCase(tableName)
+
+	err := templ.Execute(
+		buf,
+		map[string]any{
+			"CamelCasedTableName": camelCasedTableName,
+			"TableType":           camelCasedTableName + "Table",
+			"TableColsType":       camelCasedTableName + "TableCols",
+			"TableName":           tableName,
+			"Cols":                cols,
+			"PrimaryKeys":         primaryKeys,
+		},
+	)
+	return errors.Wrapf(err, "executing template for %q", tableName)
+}
+
+type Column struct {
+	Name         string
+	Type         string
+	Default      *string
+	CharLength   *int
+	Nullable     bool
+	IsPrimaryKey bool
+}
+
+func ColumnType(c Column, isGetter ...any) string {
+	switch c.Type {
+	case "bytea":
+		return "[]byte"
+	case "bool":
+		return "bool"
+	case "int8":
+		return "int64"
+	case "int2":
+		return "int16"
+	case "int4":
+		return "int32"
+	case "text", "varchar", "uuid":
+		return "string"
+	case "json", "jsonb":
+		if len(isGetter) > 0 {
+			return "json.RawMessage"
+		}
+		return "any"
+	case "float4":
+		return "float32"
+	case "float8":
+		return "float64"
+	case "date":
+		return "civil.Date"
+	case "time", "timetz":
+		return "civil.Time"
+	case "timestamp", "timestamptz":
+		return "time.Time"
+	case "numeric":
+		return "decimal.Decimal"
+	case "interval":
+		return "time.Duration"
+	default:
+		logger.With(slog.String("type", c.Type)).Error("unknown type")
+		return "any"
+	}
+}
+
+func SelectColumnType(c Column) (s string) {
+	if c.Nullable {
+		switch c.Type {
+		case "json", "jsonb", "bytea":
+		default:
+			s += "*"
+		}
+	}
+
+	s += ColumnType(c, true)
+
+	s += " // " + c.Type
+	if c.CharLength != nil {
+		s += "(" + strconv.Itoa(*c.CharLength) + ")"
+	}
+	if c.Nullable {
+		s += " null"
+	}
+	if c.Default != nil {
+		s += " default(" + *c.Default + ")"
+	}
+
+	return s
+}
+
+func InsertColumnType(c Column) (s string) {
+	if c.Nullable || c.Default != nil {
+		switch c.Type {
+		case "json", "jsonb", "bytea":
+		default:
+			s += "*"
+		}
+	}
+
+	s += ColumnType(c)
+
+	s += " // " + c.Type
+	if c.CharLength != nil {
+		s += "(" + strconv.Itoa(*c.CharLength) + ")"
+	}
+	if c.Nullable {
+		s += " null"
+	}
+	if c.Default != nil {
+		s += " default(" + *c.Default + ")"
+	}
+
+	return s
+}
+
+func UpdateColumnType(c Column) (s string) {
+	if c.Nullable || c.Default != nil {
+		switch c.Type {
+		case "json", "jsonb":
+		default:
+			s += "*"
+		}
+	} else {
+		switch c.Type {
+		case "bytea", "json", "jsonb":
+		default:
+			s += "*"
+		}
+	}
+
+	s += ColumnType(c)
+
+	s += " // " + c.Type
+	if c.CharLength != nil {
+		s += "(" + strconv.Itoa(*c.CharLength) + ")"
+	}
+	if c.Nullable {
+		s += " null"
+	}
+	if c.Default != nil {
+		s += " default(" + *c.Default + ")"
+	}
+
+	return s
+}
+
+var (
+	upperCase   = cases.Title(language.English)
+	initialisms = map[string]struct{}{"id": {}, "url": {}, "db": {}}
+)
+
+func snakeToCamelCase(s string) string {
+	var b strings.Builder
+	b.Grow(len(s))
+
+	for _, s := range strings.FieldsFunc(s, func(r rune) bool { return r == '_' || r == '.' }) {
+		if _, ok := initialisms[s]; ok {
+			b.WriteString(strings.ToUpper(s))
+		} else {
+			b.WriteString(upperCase.String(s))
+		}
+	}
+
+	return b.String()
+}
